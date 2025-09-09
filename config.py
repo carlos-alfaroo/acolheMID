725b79072e43576ef98a4a27c5a9e58feb8de692
Carlos Alfaro <carlosb.alfaro04@gmail.com>
2025-09-09 10:57:38 -0600

    Agregué detalles para la migración a postgreSQL

--- config.py
+++ config.py
@@ -4,9 +4,17 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-  SECRET_KEY = os.environ.get("SECRET_KEY") or "default_secret_key"
-  SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'app.db')}" #os.environ.get("DATABASE_URL") or "sqlite:///site.db"
+  SECRET_KEY = os.environ.get("SECRET_KEY") or "MiaTa2026@modularCode!"
+  
+  # Leer variable de entorno para PostgreSQL o fallback a SQLite local
+  uri = os.environ.get("DATABASE_URL")
+  if uri and uri.startswith("postgres://"):
+    # Render y Heroku usan postgres:// pero SQLAlchemy requiere postgresql://
+    uri = uri.replace("postgres://", "postgresql://", 1)
+  SQLALCHEMY_DATABASE_URI = uri or f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'app.db')}"
+  
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  
   UPLOAD_FOLDER = os.path.join(BASE_DIR, "app", "static", "uploads")
   MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # Max 2MB per img
   ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
